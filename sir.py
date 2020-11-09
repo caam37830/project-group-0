45eabc16cd8f230705bc9968e54e1b2c787b22bb
MoomooA <anyu0320@gmail.com>
2020-11-09 16:42:31 -0600

    individual class

--- sir.py
+++ sir.py
@@ -1,1 +1,25 @@
 # Python package includes class and function definitions
+
+
+class individual(self):
+    """
+    class to create individuals
+    the internal state of an individual can be
+    susceptible, infected, or removed
+    """
+    
+    def __init__(self, state):
+        self.state = state
+        
+    def curstate(self):
+        """
+        method to determin the state of the individual
+        """
+        return self.state
+    
+    def changestate(self, newstate):
+        """
+        method to change state of the individual
+        """
+        self.state = newstate
+        
