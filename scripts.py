458c93e5a407fca30c5f40e61fbad5d01b4bd39e
MoomooA <anyu0320@gmail.com>
2020-11-09 17:08:47 -0600

    initial commit

--- scripts.py
+++ scripts.py
@@ -1,1 +1,24 @@
 # all scripts used to run code
+
+from sir import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+# set parameters b and k here???
+# b: the number of interactions each day that could spread the disease (per individual)
+# k: the fraction of the infectious population which recovers each day
+
+b = 
+k = 
+
+# v = (s, i, r)
+def SIR(t, v):
+    return np.array([-b * v[0] * v[1], k * v[1], b * v[0] * v[1] - k * v[1]])
+
+v0 = 
+t_span = 
+t_eval = 
+
+sol = solve_ivp(SIR, t_span, v0, t_eval)
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 # unit tests
+
+import unittest
+from sir import *
+import numpy as np
+
+class Test(unittest.TestCase):
+    pass
